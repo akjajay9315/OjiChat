4b3c71983a3cdc4b35dee97fdea28232d9e51c73
Ajay kumar <akjajay9315@gmail.com>
2024-07-27 21:56:55 +0530

    message

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,19 +1,23 @@
 import { doc, onSnapshot } from "firebase/firestore";
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
-import { ChatContext } from "../context/ChatContext.js";
+import { ChatContext } from "../context/ChatContext";
 import { db } from "../firebase";
 
 const Chats = () => {
   const [chats, setChats] = useState([]);
-
   const { currentUser } = useContext(AuthContext);
   const { dispatch } = useContext(ChatContext);
 
   useEffect(() => {
+    if (!currentUser?.uid) return; // Guard clause for when currentUser is not defined
+
     const getChats = () => {
       const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
-        setChats(doc.data());
+        const data = doc.data();
+        if (data) {
+          setChats(data);
+        }
       });
 
       return () => {
@@ -21,8 +25,8 @@ const Chats = () => {
       };
     };
 
-    currentUser.uid && getChats();
-  }, [currentUser.uid]);
+    getChats();
+  }, [currentUser?.uid]);
 
   const handleSelect = (user) => {
     dispatch({ type: "CHANGE_USER", payload: user });
@@ -30,19 +34,21 @@ const Chats = () => {
 
   return (
     <div className="chats">
-      {Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat) => (
-        <div
-          className="userChat"
-          key={chat[0]}
-          onClick={() => handleSelect(chat[1].userInfo)}
-        >
-          <img src={chat[1].userInfo.photoURL} alt="" />
-          <div className="userChatInfo">
-            <span>{chat[1].userInfo.displayName}</span>
-            <p>{chat[1].lastMessage?.text}</p>
+      {Object.entries(chats)
+        ?.sort((a, b) => (b[1]?.date || 0) - (a[1]?.date || 0))
+        .map((chat) => (
+          <div
+            className="userChat"
+            key={chat[0]}
+            onClick={() => handleSelect(chat[1]?.userInfo)}
+          >
+            <img src={chat[1]?.userInfo?.photoURL || ""} alt="User Avatar" />
+            <div className="userChatInfo">
+              <span>{chat[1]?.userInfo?.displayName || "Unknown User"}</span>
+              <p>{chat[1]?.lastMessage?.text || "No message"}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
